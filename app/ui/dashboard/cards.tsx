2ab752dde9e451429f3f6d5f542a497eba27b2b0
agmt92 <155798390+agmt92@users.noreply.github.com>
2024-08-30 17:48:26 +0200

    feat: connect database, fetch data, skeletonProgress:

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -18,14 +18,14 @@ export default async function CardWrapper() {
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
